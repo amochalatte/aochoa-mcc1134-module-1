81e7c04bc5a5efe825cfab310d36843d71713a77
Amber Ochoa <amochoa@mail.mccneb.edu>
2023-04-17 20:57:32 -0500

    Assignment 1 Changes

---
+++ 2-Assignment/ochoa_assignment1.js
@@ -0,0 +1,144 @@
+'use strict';
+
+class LikeButton extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { liked: false };
+  }
+
+  render() {
+    if (this.state.liked) {
+      return 'You liked this.';
+    }
+
+    return (
+      <button onClick={() => this.setState({ liked: true })}>
+        Like
+      </button>
+    );
+  }
+}
+
+class ContactButton extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { showContactInfo: false };
+  }
+
+  render() {
+    const contactImgUrl = 'https://img1.wsimg.com/isteam/ip/5d492c18-02a5-408e-9899-878c37b9790c/ms%20amber.jpg/:/rs=w:1280,h:1707';
+    const contactInfo = 'Email: amochoa@mail.mccneb.edu';
+
+    return (
+      <div>
+        <img src={contactImgUrl} onClick={() => this.setState({ showContactInfo: !this.state.showContactInfo })} />
+        {this.state.showContactInfo && <pre>{contactInfo}</pre>}
+      </div>
+    );
+  }
+}
+
+class Clock extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { date: new Date() };
+  }
+
+  componentDidMount() {
+    this.timerID = setInterval(() => this.tick(), 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
+
+  tick() {
+    this.setState({ date: new Date() });
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>It is {this.state.date.toLocaleTimeString()}.</h1>
+      </div>
+    );
+  }
+}
+
+class TodoApp extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { items: [], text: '' };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  render() {
+    return (
+      <div>
+        <h3>TODO</h3>
+        <TodoList items={this.state.items} />
+        <form onSubmit={this.handleSubmit}>
+          <label htmlFor="new-todo">
+            What needs to be done?
+          </label>
+          <input
+            id="new-todo"
+            onChange={this.handleChange}
+            value={this.state.text}
+          />
+          <button>
+            Add #{this.state.items.length + 1}
+          </button>
+        </form>
+      </div>
+    );
+  }
+
+  handleChange(e) {
+    this.setState({ text: e.target.value });
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    if (this.state.text.length === 0) {
+      return;
+    }
+    const newItem = {
+      text: this.state.text,
+      id: Date.now()
+    };
+    this.setState(state => ({
+      items: state.items.concat(newItem),
+      text: ''
+    }));
+  }
+}
+
+class TodoList extends React.Component {
+  render() {
+    return (
+      <ul>
+        {this.props.items.map(item => (
+          <li key={item.id}>{item.text}</li>
+        ))}
+      </ul>
+    );
+  }
+}
+
+class MyComponent extends React.Component {
+  render() {
+    return (
+      <div>
+        <ContactButton />
+        <LikeButton />
+        <Clock />
+        <TodoApp />       
+      </div>
+    );
+  }
+}
+
+const domContainer = document.querySelector('#root');
+ReactDOM.render(<MyComponent />, domContainer);
